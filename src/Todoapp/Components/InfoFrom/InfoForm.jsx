811aeb0d29b260a3a94d80f80727f5b07c0535c7
Sudhanshu G Gaikwad <140872777+sggaikwad@users.noreply.github.com>
2024-07-12 21:27:19 +0530

    Resolved merge conflicts

--- src/Todoapp/Components/InfoFrom/InfoForm.jsx
+++ src/Todoapp/Components/InfoFrom/InfoForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import "./Style.css";
+import styled from "styled-components";
+import Navigation from "../Navigation/Navigation";
 function InfoForm() {
   const [name, setName] = useState("");
   const [mobile, setMobile] = useState("");
@@ -8,100 +9,138 @@ function InfoForm() {
 
   return (
     <>
-      <div className="Container">
-        <div>
-          <div className="col-12 Heading">
-            <h2 className="title position-relative pb-2 mb-4">
-              {" "}
-              | INFORMATION - 01 |
-            </h2>
-          </div>
-          <div className="col-12">
-            <div className="contact-form">
-              <div id="success"></div>
-              <form name="sentMessage" id="contactForm" novalidate="novalidate">
-                <div className="form-row">
-                  <div className="control-group col-sm-6 INput mb-3">
-                    <input
-                      type="text"
-                      className="form-control p-4"
-                      id="name"
-                      placeholder="Name"
-                      required="required"
-                      data-validation-required-message="Please enter your name"
-                      name="Name"
-                      onChange={(e) => {
-                        setName(e.target.value);
-                      }}
-                    />
-                  </div>
+      <MainConatiner>
+        <Conatiner>
+          <Navigation />
 
-                  <div className="control-group col-sm-6 INput mb-3">
-                    <input
-                      type="text"
-                      className="form-control p-4"
-                      id="email"
-                      placeholder="Mobile Number"
-                      required="required"
-                      name="Mobile"
-                      onChange={(e) => {
-                        setMobile(e.target.value);
-                      }}
-                    />
-                  </div>
+          <div className="container mt-3">
+            <div className="row mt-4 mb-4">
+              <h4 className="info-headding"> Live Information Form</h4>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <div className="col-6">
+                  <div className="contact-form">
+                    <div id="success"></div>
+                    <form
+                      name="sentMessage"
+                      id="contactForm"
+                      novalidate="novalidate"
+                    >
+                      <div className="form-row">
+                        <div className="control-group col-sm-6 INput mb-3">
+                          <input
+                            type="text"
+                            className="todo-inp"
+                            id="name"
+                            placeholder="Name"
+                            required="required"
+                            data-validation-required-message="Please enter your name"
+                            name="Name"
+                            onChange={(e) => {
+                              setName(e.target.value);
+                            }}
+                          />
+                        </div>
 
-                  <div className="control-group col-sm-6 INput mb-3">
-                    <input
-                      type="text"
-                      className="form-control p-4"
-                      id="email"
-                      placeholder="Email"
-                      required="required"
-                      name="Email"
-                      onChange={(e) => {
-                        setEmaile(e.target.value);
-                      }}
-                    />
-                  </div>
-                  <div className="control-group col-sm-6 INput mb-3">
-                    <input
-                      type="text"
-                      className="form-control p-4"
-                      id="address"
-                      placeholder="Address"
-                      required="required"
-                      name="Email"
-                      onChange={(e) => {
-                        setAddress(e.target.value);
-                      }}
-                    />
+                        <div className="control-group col-sm-6 INput mb-3">
+                          <input
+                            type="text"
+                            className="todo-inp"
+                            id="email"
+                            placeholder="Mobile Number"
+                            required="required"
+                            name="Mobile"
+                            onChange={(e) => {
+                              setMobile(e.target.value);
+                            }}
+                          />
+                        </div>
+
+                        <div className="control-group col-sm-6 INput mb-3">
+                          <input
+                            type="text"
+                            className="todo-inp"
+                            id="email"
+                            placeholder="Email"
+                            required="required"
+                            name="Email"
+                            onChange={(e) => {
+                              setEmaile(e.target.value);
+                            }}
+                          />
+                        </div>
+                        <div className="control-group col-sm-6 INput mb-3">
+                          <input
+                            type="text"
+                            className="todo-inp"
+                            id="address"
+                            placeholder="Address"
+                            required="required"
+                            name="Email"
+                            onChange={(e) => {
+                              setAddress(e.target.value);
+                            }}
+                          />
+                        </div>
+                      </div>
+                    </form>
                   </div>
                 </div>
-              </form>
+              </div>
+
+              <div className="col-md-6">
+                <div className="">
+                  <p className="info-text">
+                    Name : <span className="mx-2">{name}</span>
+                  </p>
+                  <p className="info-text">
+                    Mobile Number : <span className="mx-2">{mobile}</span>
+                  </p>
+                  <p className="info-text">
+                    Email : <span className="mx-2">{email}</span>
+                  </p>
+                  <p className="info-text">
+                    Address : <span className="mx-2">{address}</span>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="RightContianer">
-          <h1>| INFORMATION - 02 | </h1>
-          <div className="mt-4 Info">
-            <p className="help-block  mt-3 ">
-              | Name :- <span>{name}</span>
-            </p>
-            <p className="help-block  mt-3 ">
-              | Mobile Number :- <span>{mobile}</span>
-            </p>
-            <p className="help-block  mt-3 ">
-              | Email :- <span>{email}</span>
-            </p>
-            <p className="help-block  mt-3 ">
-              | Address :- <span>{address}</span>
-            </p>
-          </div>
-        </div>
-      </div>
+        </Conatiner>
+      </MainConatiner>
     </>
   );
 }
 
+const MainConatiner = styled.div`
+  background-image: url("https://images.pexels.com/photos/4840134/pexels-photo-4840134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+`;
+const Conatiner = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100vh;
+`;
+
+const Button = styled.button`
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 5px 40px 5px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border: 2px solid rgb(255, 255, 255);
+  width: 40%;
+  &:hover {
+    background-color: #45a049;
+  }
+`;
 export default InfoForm;
