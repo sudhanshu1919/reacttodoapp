31c922c91935efe262ec7e5c5e2ed862b4c0ce8d
Sudhanshu G Gaikwad <140872777+sggaikwad@users.noreply.github.com>
2024-07-13 15:29:03 +0530

    Resolved merge conflicts

--- src/Todoapp/App.js
+++ src/Todoapp/App.js
@@ -1,38 +1,109 @@
+// import React, { useState } from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navigation from "./Components/Navigation/Navigation";
+// import MainToDo from "./Components/TodoApp/MainToDo/MainToDo";
+// import Carousel from "./Components/Carousel/Carousel";
+// import InfoForm from "./Components/InfoFrom/InfoForm";
+
+// import Nopage from "./Components/Nopage/Nopage";
+
+// function App(value) {
+//   const [data, setData] = useState([]);
+
+//   // This is for edite data in the todo items..1
+//   const [isEditing, setEditing] = useState(false);
+
+//   const addData = (item) => {
+//     // alert(item);
+//     setData([...data, item]);
+//   };
+
+//   const deleteData = (value) => {
+//     // alert(value);
+//     const newData = data.filter((item) => item !== value);
+//     setData(newData);
+//   };
+
+//   const updateData = () => {
+//     let newData = data.map((item, i) => {
+//       if (i === value.index) {
+//         return value.newv;
+//       } else {
+//         return item;
+//       }
+//     });
+//     console.log(newData);
+//     setData(newData);
+//   };
+
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <MainToDo
+//                 data={data}
+//                 addData={addData}
+//                 deleteData={deleteData}
+//                 isEditing={isEditing}
+//                 setEditing={setEditing}
+//                 updateData={updateData}
+//               />
+//             }
+//           />
+//           <Route
+//             path="/MainToDo"
+//             element={
+//               <MainToDo
+//                 data={data}
+//                 addData={addData}
+//                 deleteData={deleteData}
+//                 isEditing={isEditing}
+//                 setEditing={setEditing}
+//                 updateData={updateData}
+//               />
+//             }
+//           />
+//           <Route path="/Carousel" element={<Carousel />} />
+//           <Route path="/InfoForm" element={<InfoForm />} />
+//           <Route path="*" element={<Nopage />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./Components/Navigation/Navigation";
 import MainToDo from "./Components/TodoApp/MainToDo/MainToDo";
 import Carousel from "./Components/Carousel/Carousel";
 import InfoForm from "./Components/InfoFrom/InfoForm";
-
 import Nopage from "./Components/Nopage/Nopage";
 
-function App(value) {
+function App() {
   const [data, setData] = useState([]);
 
-  // This is for edite data in the todo items..1
+  // This is for edit data in the todo items
   const [isEditing, setEditing] = useState(false);
 
   const addData = (item) => {
-    // alert(item);
     setData([...data, item]);
   };
 
   const deleteData = (value) => {
-    // alert(value);
     const newData = data.filter((item) => item !== value);
     setData(newData);
   };
 
-  const updateData = () => {
-    let newData = data.map((item, i) => {
-      if (i === value.index) {
-        return value.newv;
-      } else {
-        return item;
-      }
-    });
-    console.log(newData);
+  const updateData = (updatedData) => {
+    const newData = data.map((item, i) =>
+      i === updatedData.index ? updatedData.newv : item
+    );
     setData(newData);
   };
 
--- src/Todoapp/Components/TodoApp/Items/Items.jsx
+++ src/Todoapp/Components/TodoApp/Items/Items.jsx
@@ -1,3 +1,121 @@
+// import React, { useState } from "react";
+// import Edit from "../../../assets/images/edit.png";
+// import Delete from "../../../assets/images/delete.png";
+// import Right from "../../../assets/images/right.png";
+// import Wrong from "../../../assets/images/x-mark.png";
+// import styled from "styled-components";
+
+// function Items(props) {
+//   const [isEditing, setEditing] = useState(false);
+//   const [inpText, setInpText] = useState(props.item);
+
+//   const handleUpdate = () => {
+//     const updatedData = {
+//       index: props.index,
+//       prev: props.item,
+//       newv: inpText,
+//     };
+//     props.updateData(updatedData);
+//     setEditing(false);
+//   };
+
+//   const handleCancel = () => {
+//     setInpText(props.item); // Reset the input text to the original value
+//     setEditing(false);
+//   };
+
+//   return (
+//     <div className="item-container">
+//       {isEditing ? (
+//         <ItmCon>
+//           <div>
+//             <input
+//               className="todo-inp2"
+//               type="text"
+//               value={inpText}
+//               onChange={(e) => setInpText(e.target.value)}
+//             />
+//           </div>
+//           <div>
+//             <Button onClick={handleUpdate} className="mx-1">
+//               <img src={Right} width="25px" alt="Right" />
+//             </Button>
+//             <Button onClick={handleCancel}>
+//               <img src={Wrong} width="25px" alt="Wrong" />
+//             </Button>
+//           </div>
+//         </ItmCon>
+//       ) : (
+//         <ItmCon2>
+//           <div>
+//             <p className="todo-inp3 mt-2">{props.item}</p>
+//           </div>
+//           <div>
+//             <Button className="mx-1" onClick={() => setEditing(true)}>
+//               <img src={Edit} width="30px" alt="Edit" />
+//             </Button>
+//             <Button
+//               className="mt-2"
+//               onClick={() => props.deleteData(props.item)}
+//             >
+//               <img src={Delete} width="30px" alt="Delete" />
+//             </Button>
+//           </div>
+//         </ItmCon2>
+//       )}
+//     </div>
+//   );
+// }
+
+// const ItmCon = styled.div`
+//   display: flex;
+//   margin-top: 30px;
+
+//   @media (max-width: 768px) {
+//     flex-direction: column;
+//     align-items: center;
+//   }
+// `;
+
+// const Button = styled.button`
+//   background-color: #4caf50;
+//   border: none;
+//   color: white;
+//   padding: 5px 28px;
+//   text-align: center;
+//   text-decoration: none;
+//   display: inline-block;
+//   font-size: 16px;
+//   cursor: pointer;
+//   border: 2px solid rgb(255, 255, 255);
+
+//   &:hover {
+//     background-color: #45a049;
+//   }
+
+//   @media (max-width: 768px) {
+//     padding: 5px 20px;
+//     font-size: 14px;
+//   }
+
+//   @media (max-width: 480px) {
+//     padding: 5px 15px;
+//     font-size: 12px;
+//   }
+// `;
+
+// const ItmCon2 = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+
+//   @media (max-width: 768px) {
+//     flex-direction: column;
+//     align-items: center;
+//   }
+// `;
+
+// export default Items;
+
 import React, { useState } from "react";
 import Edit from "../../../assets/images/edit.png";
 import Delete from "../../../assets/images/delete.png";
@@ -87,7 +205,7 @@ const Button = styled.button`
   display: inline-block;
   font-size: 16px;
   cursor: pointer;
-  border: 2px solid rgb(255, 255, 255);
+  border: 1px solid rgb(255, 255, 255);
 
   &:hover {
     background-color: #45a049;
