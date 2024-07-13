405c6359457ecaba4cd72242480ef52204c8c3ea
Sudhanshu G Gaikwad <140872777+sggaikwad@users.noreply.github.com>
2024-07-13 17:01:07 +0530

    Resolved merge conflicts

--- src/Todoapp/Components/Carousel/Carousel.jsx
+++ src/Todoapp/Components/Carousel/Carousel.jsx
@@ -1,26 +1,152 @@
+// import React, { useState } from "react";
+// import { sculptureList } from "./data.js";
+// import styled from "styled-components";
+// import Navigation from "../Navigation/Navigation";
+// import Footer from "../Footer/Footer.jsx";
+// function Carousel() {
+//   //   console.log(sculptureList);
+
+//   const [index, setIndex] = useState(0);
+
+//   // wee have give data from useState Hook in the new variable..!
+//   let product = sculptureList[index];
+
+//   // This is show more button
+//   const [showmore, setShoMore] = useState(false);
+//   let hasnext = true;
+
+//   // this is for Next Buttons
+//   function handleClick() {
+//     setIndex(index + 1);
+//   }
+
+//   // this is for Privious Button
+
+//   function handelClickPrev() {
+//     setIndex(index - 1);
+//   }
+
+//   return (
+//     <>
+//       <MainConatiner>
+//         <Conatiner>
+//           <Navigation />
+//           <MainCard>
+//             <div>
+//               <h4 className="text-center text-white mb-3">Shopping Card</h4>
+//               <div className="MainCard" style={{ width: "20rem" }}>
+//                 <img
+//                   style={{ width: "250px" }}
+//                   className="card-img-top"
+//                   src={product.image}
+//                   alt="Card image cap"
+//                 />
+//                 <div className="card-body mt-3">
+//                   <div>
+//                     <h5>{product.Name}</h5>
+//                     <span>
+//                       Products <b>{index + 1}</b> of{" "}
+//                       <b>{sculptureList.length}</b>
+//                     </span>
+//                   </div>
+
+//                   <div>
+//                     <span className="card-title">{product.Category}</span>
+//                     <span className="card-title">
+//                       <b>{product.price}</b>
+//                     </span>
+//                   </div>
+
+//                   <Button
+//                     className="mt-2"
+//                     onClick={() => {
+//                       setShoMore(!showmore);
+//                     }}
+//                   >
+//                     See Information
+//                   </Button>
+//                   <p>{showmore && product.About}</p>
+//                 </div>
+
+//                 <div
+//                   className="card-body"
+//                   style={{ display: "flex", justifyContent: "space-between" }}
+//                 >
+//                   <Button
+//                     onClick={handelClickPrev}
+//                     disabled={index <= 0 ? true : false}
+//                   >
+//                     Previous
+//                   </Button>
+//                   <Button
+//                     onClick={handleClick}
+//                     disabled={index >= sculptureList.length - 1 ? true : false}
+//                   >
+//                     Next
+//                   </Button>
+//                 </div>
+//               </div>
+//             </div>
+//           </MainCard>
+
+//           <Footer />
+//         </Conatiner>
+//       </MainConatiner>
+//     </>
+//   );
+// }
+
+// const MainConatiner = styled.div`
+//   background-image: url("https://images.pexels.com/photos/4840134/pexels-photo-4840134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+//   background-repeat: no-repeat;
+//   background-size: cover;
+//   width: 100%;
+//   height: 100vh;
+// `;
+// const Conatiner = styled.div`
+//   background-color: rgba(0, 0, 0, 0.5);
+//   width: 100%;
+//   height: 100vh;
+// `;
+
+// const MainCard = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   margin-top: 70px;
+// `;
+
+// const Button = styled.button`
+//   background-color: #4caf50;
+//   border: none;
+//   color: white;
+//   padding: 5px 40px 5px;
+//   text-align: center;
+//   text-decoration: none;
+//   display: inline-block;
+//   font-size: 16px;
+//   margin: 4px 2px;
+//   cursor: pointer;
+//   border: 2px solid rgb(255, 255, 255);
+//   &:hover {
+//     background-color: #45a049;
+//   }
+// `;
+// export default Carousel;
+
 import React, { useState } from "react";
 import { sculptureList } from "./data.js";
 import styled from "styled-components";
 import Navigation from "../Navigation/Navigation";
 import Footer from "../Footer/Footer.jsx";
+
 function Carousel() {
-  //   console.log(sculptureList);
-
   const [index, setIndex] = useState(0);
-
-  // wee have give data from useState Hook in the new variable..!
   let product = sculptureList[index];
-
-  // This is show more button
   const [showmore, setShoMore] = useState(false);
-  let hasnext = true;
 
-  // this is for Next Buttons
   function handleClick() {
     setIndex(index + 1);
   }
-
-  // this is for Privious Button
 
   function handelClickPrev() {
     setIndex(index - 1);
@@ -32,11 +158,10 @@ function Carousel() {
         <Conatiner>
           <Navigation />
           <MainCard>
-            <div>
-              <h4 className="text-center text-white mb-3">Shopping Card</h4>
-              <div className="MainCard" style={{ width: "20rem" }}>
+            <div className="MainCard">
+              <h4 className="text-center mb-3">Books</h4>
+              <Card>
                 <img
-                  style={{ width: "250px" }}
                   className="card-img-top"
                   src={product.image}
                   alt="Card image cap"
@@ -63,29 +188,23 @@ function Carousel() {
                       setShoMore(!showmore);
                     }}
                   >
-                    See Information
+                    About information
                   </Button>
                   <p>{showmore && product.About}</p>
                 </div>
 
-                <div
-                  className="card-body"
-                  style={{ display: "flex", justifyContent: "space-between" }}
-                >
-                  <Button
-                    onClick={handelClickPrev}
-                    disabled={index <= 0 ? true : false}
-                  >
+                <div className="card-body buttons-container">
+                  <Button onClick={handelClickPrev} disabled={index <= 0}>
                     Previous
                   </Button>
                   <Button
                     onClick={handleClick}
-                    disabled={index >= sculptureList.length - 1 ? true : false}
+                    disabled={index >= sculptureList.length - 1}
                   >
                     Next
                   </Button>
                 </div>
-              </div>
+              </Card>
             </div>
           </MainCard>
 
@@ -103,6 +222,7 @@ const MainConatiner = styled.div`
   width: 100%;
   height: 100vh;
 `;
+
 const Conatiner = styled.div`
   background-color: rgba(0, 0, 0, 0.5);
   width: 100%;
@@ -115,11 +235,41 @@ const MainCard = styled.div`
   margin-top: 70px;
 `;
 
+const Card = styled.div`
+  width: 20rem;
+
+  img {
+    width: 50%;
+    border-radius: 10px;
+  }
+
+  .buttons-container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  @media (max-width: 768px) {
+    width: 15rem;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 480px) {
+    width: 10rem;
+
+    img {
+      width: 100%;
+    }
+  }
+`;
+
 const Button = styled.button`
   background-color: #4caf50;
   border: none;
   color: white;
-  padding: 5px 40px 5px;
+  padding: 5px 20px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
@@ -130,5 +280,16 @@ const Button = styled.button`
   &:hover {
     background-color: #45a049;
   }
+
+  @media (max-width: 768px) {
+    padding: 5px 15px;
+    font-size: 14px;
+  }
+
+  @media (max-width: 480px) {
+    padding: 5px 10px;
+    font-size: 12px;
+  }
 `;
+
 export default Carousel;
--- src/Todoapp/Components/Carousel/data.js
+++ src/Todoapp/Components/Carousel/data.js
@@ -1,128 +1,42 @@
 export const sculptureList = [
   {
     id: "1",
-    image:
-      "https://media.sofa.com/thumbor/unsafe/fit-in/440x220/center/middle/https%3A%2F%2Fmedia.sofa.com%2Fmedias%2F%3Fcontext%3DbWFzdGVyfHJvb3R8MTk5MDU5fGltYWdlL2pwZWd8aGMxL2gyYi85MDE1NTUzNjU0ODE0LmpwZ3w3M2IyYmQzZmE3M2U2YmQ4ZDg5N2EyNTAwOWE0NjI2OWJlZmUyM2YzNTIxMmM3ZTU3ZDZkZjg1ODgyNzg4ODU4",
-    Name: "Bowie Alderney Sofa",
-    price: "₹ : 25,000/-",
-    Category: "Sofa",
+    image: "https://m.media-amazon.com/images/I/51pShm9m+AL.jpg",
+    Name: "The Science of Self-Learning",
+    price: "₹ : 200/-",
     About:
-      "Classic, effortlessly good looks, and timeless appeal, our collection of traditional armchairs offers supreme comfort and style.",
+      "How to Teach Yourself Anything, Learn More in Less Time, and Direct Your Own Education",
   },
   {
     id: "2",
-    image: "https://m.media-amazon.com/images/I/71H7tMBS8CL._SL1500_.jpg",
-    Name: "boAt Airdopes",
-    price: "₹ : 1,499/-",
-    Category: "Airdopes",
+    image: "https://m.media-amazon.com/images/I/51Wlt2a6q4L.jpg",
+    Name: "The Smart Habit Guide",
+    price: "₹ : 1199/-",
     About:
-      "boAt Airdopes 141 Bluetooth TWS in Ear Earbuds with 42H Playtime,Low Latency Mode for Gaming, ENx Tech, IWP, IPX4 Water Resistance, Smooth Touch...",
+      "The Smart Habit Guide: 37 Small Life Changes Your Brain Will Thank You for Making (Master Your Mind, Revolutionize Your Life Series)",
   },
   {
     id: "3",
-    image: "https://m.media-amazon.com/images/I/81FsR58fGwL._SL1500_.jpg",
-    Name: "Bluebell Armchair",
-    price: "₹ : 55,999/-",
-    Category: "Chair",
-    About:
-      "Classic, effortlessly good looks, and timeless appeal, our collection of traditional armchairs offers supreme comfort and style.",
+    image: "https://m.media-amazon.com/images/I/419Im9cM7iL.jpg",
+    Name: "On Meditation",
+    price: "₹ : 599/-",
+    About: "On Meditation: Finding Infinite Bliss and Power Within",
   },
   {
     id: "4",
-    image:
-      "https://media.sofa.com/thumbor/unsafe/fit-in/440x220/center/middle/https%3A%2F%2Fmedia.sofa.com%2Fmedias%2F%3Fcontext%3DbWFzdGVyfHJvb3R8Nzg5OTgwfGltYWdlL2pwZWd8aGZmL2g1MC84OTAzOTUyMTA1NTAyLmpwZ3xkMmFlMzIyOGRmMTRiZTkxMGI5OTFmYTU3NGQxNzJmMzZhZDViYzVlMGM2YTAxNGI3NDBkNWUxMzMwNjU3YzZj",
-    Name: "Duke Armchair",
-    price: "₹ : 31,000/-",
-    Category: "Chair",
+    image: "https://m.media-amazon.com/images/I/51EG8AyJiQL.jpg",
+    Name: "How to Read a Book",
+    price: "₹ : 1,000/-",
     About:
-      "Classic, effortlessly good looks, and timeless appeal, our collection of traditional armchairs offers supreme comfort and style.",
+      "How to Read a Book was first published in the early months of 1940.",
   },
   {
     id: "5",
     image:
-      "https://media.sofa.com/thumbor/unsafe/fit-in/440x220/center/middle/https%3A%2F%2Fmedia.sofa.com%2Fmedias%2F%3Fcontext%3DbWFzdGVyfHJvb3R8MjIyNzd8aW1hZ2UvanBlZ3xoMjYvaDYzLzg4NTYzMzQyMzc3MjYuanBnfGZjYjgyYjRiODQ1ZmI4ZDJmNGEyZjU3MDc2N2I0NWUxNDA1ODY1OTc5MGNkNDMzZjA2YTk3NTIyMWRkODZjNDA",
-    Name: "Betty Armchair",
-    price: "₹ : 67,000/-",
-    Category: "Chair",
+      "https://m.media-amazon.com/images/P/B077NB5H6Y.01._SCLZZZZZZZ_SX500_.jpg",
+    Name: "Learning JavaScript",
+    price: "₹ : 1800/-",
     About:
-      "Classic, effortlessly good looks, and timeless appeal, our collection of traditional armchairs offers supreme comfort and style.",
-  },
-  {
-    id: "6",
-    image: "https://m.media-amazon.com/images/I/71LfnkRgZ4L._SL1500_.jpg",
-    Name: "Apple Watch SE",
-    price: "₹ : 29,999/-",
-    Category: "Watch",
-    About:
-      "Apple Watch SE (2nd Gen) [GPS 40 mm] Smart Watch w/Silver Aluminium Case & White Sport Band. Fitness & Sleep Tracker, Crash Detection, Heart Rate Monitor, Retina Display,",
-    Services: "1 Year Warranty,No Returns Applicable,GST invoice available",
-  },
-  {
-    id: "7",
-    image:
-      "https://media.sofa.com/thumbor/unsafe/fit-in/440x220/center/middle/https%3A%2F%2Fmedia.sofa.com%2Fmedias%2F%3Fcontext%3DbWFzdGVyfHJvb3R8ODUxODd8aW1hZ2UvanBlZ3xoMmQvaDAzLzg5MDkxNTQ5NzU3NzQuanBnfDAwZWQzNTFiYjlkZDc5ODVhZjk5NDBjNmEzOTM0NmNlYjYyODlmNDZjYWVjYzA2ZGJiZTNhOTY1MzgwMTY3ZGI",
-    Name: "Cecil Dog Bed",
-    price: "₹: 6,000/-",
-    Category: "Petbed",
-
-    About:
-      " This petbed raises Currency question in British India, which led to the Creation of Reserve Bank of India.THE PROBLEM OF THE RUPEE was first published in 1923.",
-  },
-  {
-    id: "8",
-    image:
-      "https://media.sofa.com/thumbor/unsafe/fit-in/440x220/center/middle/https%3A%2F%2Fmedia.sofa.com%2Fmedias%2F%3Fcontext%3DbWFzdGVyfHJvb3R8NTY1MzN8aW1hZ2UvanBlZ3xoODUvaDEyLzg4NTYwMTE4NjYxNDIuanBnfGUxZGRlMjY5M2ViNWZhNTEyZGIwMTExNDcwZDk4NDk2YmFhN2M4Yjc0NzYwMDEyNDI1MDQxMDY2ZWI2MjRkYWM",
-    Name: "Knightsbridge With Footboard Bed",
-    price: "₹ : 30,999/-",
-    Category: "Bed",
-    About: "There really is nothing better than a great night’s sleep.  ",
-  },
-  {
-    id: "9",
-    image:
-      "https://www.apple.com/v/airpods-max/e/images/overview/design_colors_pink_front__dbqafvrvcy6a_xlarge.jpg",
-    Name: "APPLE New AirPods ",
-    price: "₹ : 44,999/-",
-    Category: "Airpods",
-    About:
-      ",Apple-designed dynamic driver provides high-fidelity audio,Active Noise Cancellation blocks outside noise, so you can immerse yourself in music,",
-  },
-  {
-    id: "10",
-    image:
-      "https://media.sofa.com/thumbor/unsafe/fit-in/440x220/center/middle/https%3A%2F%2Fmedia.sofa.com%2Fmedias%2F%3Fcontext%3DbWFzdGVyfHJvb3R8MTUzNDQyfGltYWdlL2pwZWd8aGRmL2gwYi85MDE0NTI1Mzk0OTc0LmpwZ3xhOWNiYjI3YzMwOTg5N2YwMGFkMzRhNjY3ZmRkMWI2ZDVlNTQ2MDhmYjcxMzdkMWFiMDA4MTEyNDNmNzgzZWFl",
-    Name: "Jack Armchair",
-    price: "₹ : 39,000/-",
-    Category: "Chair",
-    About:
-      "Classic, effortlessly good looks, and timeless appeal, our collection of traditional armchairs offers supreme comfort and style.",
-  },
-  {
-    id: "11",
-    image:
-      "https://www.apple.com/v/airpods-max/e/images/overview/design_colors_pink_front__dbqafvrvcy6a_xlarge.jpg",
-    Name: "APPLE New AirPods ",
-    price: "₹ : 44,999/-",
-    quantity: "1",
-    Category: "Airpods",
-    About:
-      ",Apple-designed dynamic driver provides high-fidelity audio,Active Noise Cancellation blocks outside noise, so you can immerse yourself in music,",
-    Services: "1 Year Warranty,No Returns Applicable,GST invoice available",
-  },
-  {
-    id: "12",
-    image: "https://m.media-amazon.com/images/I/81lNRdMarjL._SL1500_.jpg",
-    Name: "TIED RIBBONS Lord Buddha Meditating Statue Idol Showpiece",
-    price: "₹ : 27,000/-",
-    Category: "Phone",
-    About:
-      "17.00 cm (6.7-inch) Super Retina XDR display featuring Always-On and ProMotion,Dynamic Island",
-  },
-  {
-    id: "13",
-    image:
-      "https://media.istockphoto.com/id/1397892955/photo/thank-you-message-for-card-presentation-business-expressing-gratitude-acknowledgment-and.jpg?s=612x612&w=0&k=20&c=7Lyf2sRAJnX_uiDy3ZEytmirul8pyJWm4l2fxiUtdvk=",
-    Name: "Thank You",
-    About: "It was a basic shopping card..!",
+      "Learning JavaScript Data Structures and Algorithms: Write complex and powerful JavaScript code using the latest",
   },
 ];
